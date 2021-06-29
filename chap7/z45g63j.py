4196d74b6658dc59466ba4e138463b900b17978f
itc-n21019 <83333455+itc-n21019@users.noreply.github.com>
2021-06-29 19:55:03 +0900

    Update and rename a to z45g63j.py

--- chap7/z45g63j.py
+++ chap7/z45g63j.py
@@ -1,10 +1,7 @@
-tv = ["GOT", "Narcos",
-      "Vice"]
+tv = ["GOT", "Narcos", "Vice"]
 for i, show in enumerate(tv):
     new_show = tv[i]
     new_show = new_show.upper()
     tv[i] = new_show
 
-
 print(tv)
-
